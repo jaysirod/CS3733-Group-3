279b4f52e2d36284fc1e77e87eeb70df2fa9936e
Mario De Jesus <>
2021-11-17 13:43:24 -0600

    bug fixes to modifying data and better handeling of errors

--- Backend/update_staff.py
+++ Backend/update_staff.py
@@ -11,6 +11,17 @@ def update_staff(UID,first_name,last_name,email,phone_number,salary):
         print('[!] Accessing Database!')
 
         conn = sqlite3.connect('/usr/src/app/Backend/Database/test_DB.db')
+
+        cursor = conn.execute("SELECT UID , EMAIL from ADMIN WHERE EMAIL='"+email+"'")
+        admin_email = cursor.fetchall()
+        if len(admin_email) != 0:
+            if admin_email[0][0] != UID:
+                conn.close()
+                return "1"
+
+
+
+
         cursor = conn.execute("UPDATE ADMIN SET FIRST_NAME = '"+first_name+"', LAST_NAME = '"+last_name+"',EMAIL='"+email+"' ,PHONE_NUM='"+phone_number+"',SALARY ='"+salary+"' WHERE UID="+str(UID))
         conn.commit()
         conn.close()
--- Backend/update_user_information.py
+++ Backend/update_user_information.py
@@ -10,6 +10,15 @@ def update(UID,user_email,first_name,last_name,phone_number):
         print('[!] Accessing Database!')
 
         conn = sqlite3.connect('/usr/src/app/Backend/Database/test_DB.db')
+
+        cursor = conn.execute("SELECT UID , EMAIL from USERS WHERE EMAIL='"+user_email+"'")
+        email_user = cursor.fetchall()
+        if len(email_user) != 0:
+            if email_user[0][0] != UID:
+                conn.close()
+                return "1"
+
+
         cursor = conn.execute("UPDATE USERS SET FIRST_NAME = '"+first_name+"', LAST_NAME = '"+last_name+"',EMAIL='"+user_email+"' ,PHONE_NUM='"+phone_number+"' WHERE UID="+str(UID))
         conn.commit()
         conn.close()
